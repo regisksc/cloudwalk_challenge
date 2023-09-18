015f7a76e2be616967cf8af6cc6f0e7472e406d7
regisksc <regisksc@gmail.com>
2023-09-18 04:41:46 -0300

    fix(app): removes unnecessary import

--- app/test/presentation/concert_list_page_test.dart
+++ app/test/presentation/concert_list_page_test.dart
@@ -1,6 +1,5 @@
 import 'package:cloudwalk_challenge/presentation/presentation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
